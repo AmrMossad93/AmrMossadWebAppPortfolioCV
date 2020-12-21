6296b68283f885276a6749f72e76bdafdc6dfed0
AmrMossad93 <amr@unilearn.com.eg>
2020-12-21 14:36:02 +0200

    Projects

--- src/app/Models/project.ts
+++ src/app/Models/project.ts
@@ -2,5 +2,11 @@ export interface IProject {
     id: number;
     projectName: string;
     type: string;
+    projectDetail: string;
+    coreDevelopment: string;
+    client: string;
+    category: string;
+    webSiteURL: string;
+    vedioUrl: string;
     images: string[];
 }
--- src/app/Pages/main-page/project-details/project-details.component.ts
+++ src/app/Pages/main-page/project-details/project-details.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {ProjectsService} from '../../../Services/projects.service';
+import {IProject} from '../../../Models/project';
 
 @Component({
-  selector: 'app-project-details',
-  templateUrl: './project-details.component.html',
-  styleUrls: ['./project-details.component.scss']
+    selector: 'app-project-details',
+    templateUrl: './project-details.component.html',
+    styleUrls: ['./project-details.component.scss']
 })
 export class ProjectDetailsComponent implements OnInit {
+    projectId: number;
+    projects: IProject[];
+    project: IProject;
 
-  constructor() { }
+    constructor(private route: ActivatedRoute, private projectsService: ProjectsService) {
+        this.route.params.subscribe(params => {
+            this.projectId = +params.id;
+        });
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.getProjectById();
+    }
+
+    getProjectById() {
+        this.projectsService.getProjects().subscribe(res => {
+            this.projects = res as IProject[];
+        }, error => {
+        }, () => {
+            this.project = this.projects.filter(c => c.id === this.projectId)[0];
+        });
+    }
 
 }
